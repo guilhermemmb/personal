a75fcb80e3572a762faeec311ef0e67c1da94ea7
Guilherme Bomfim <guilhermemmb@gmail.com>
2014-12-16 02:38:03 -0200

    adicionando os scripts de instalacao de um pc novo

---
+++ install_script.sh
@@ -0,0 +1,26 @@
+## TOOLS
+brew install caskroom/cask/brew-cask
+
+brew install wget
+brew install python
+brew install ruby
+brew install node
+brew install git
+brew install ack
+brew install tree
+brew install trash 
+brew install rename
+
+##
+#	ZSH
+##
+#brew instal zsh
+#brew instal zsh-completions
+#brew instal zsh-syntax-highlithing
+
+
+
+brew cleanup
+
+
+pip install markup
