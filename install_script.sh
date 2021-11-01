219535e066b139cf014f2fb4e49dd9d02a6c768a
Guilherme Bomfim <guilhermemmb@gmail.com>
2021-11-01 20:37:22 -0300

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,26 +1,41 @@
-## TOOLS
-brew install caskroom/cask/brew-cask
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/guilherme.bomfim/.profile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+echo "installing dev tools..."
 brew install wget
 brew install python
-brew install ruby
-brew install node
 brew install git
+brew install docker
+brew install nvm
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install zsh-completions
+brew install zsh-syntax-highlithing
 brew install ack
 brew install tree
 brew install trash 
 brew install rename
 
-##
-#	ZSH
-##
-#brew instal zsh
-#brew instal zsh-completions
-#brew instal zsh-syntax-highlithing
+#nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
+echo "installing apps..."
+brew install cask
+brew install brew-cask-completion
+brew tap homebrew/cask
 
-
-brew cleanup
-
-
-pip install markup
+brew install --cask slack
+brew install --cask zoom
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask postman
+brew install --cask spotify
+brew install --cask tunnelblick
+brew install --cask figma
+brew install --cask krisp
+brew install --cask snapcamera
+brew install --cask hype
+brew install --cask rectangle
